541e42595611c681578e8bc5224de5f2e3cc5cb6
Lautaro-L <lautilazo@gmail.com>
2022-07-08 19:43:28 -0300

    inicio de clock, falta clock m y hacer una funcion que haga el pasamanos de la pagina q se va

--- Memoria/Memoria.c
+++ Memoria/Memoria.c
@@ -4,6 +4,7 @@ void* escuchar(int, t_config* );
 void* recibiendo(void*, t_config* );
 void iniciar_estructuras();
 
+int cantidad_de_marcos_totales;
 int tam_memoria;
 int tam_pagina;
 int entradas_por_tabla;
@@ -13,6 +14,7 @@ int cliente_kernel;
 void* espacio_memoria_user;
 int marcos_por_proceso;
 char* path_swap;
+char* alg_reemplazo;
 
 t_list* lista_global_de_tablas_de_1er_nivel;
 t_list* lista_global_de_tablas_de_2do_nivel;
@@ -27,19 +29,20 @@ typedef struct{
 } swap_struct;
 
 typedef struct{
+	uint32_t pagina_primer_nivel;
 	uint32_t offset; //numero entero que representa la posicion en el vector siguiente
 	int* marcos_asignados; // importante al ser un puntero no inicializado tener cuidado al trabajarlo, si no se inicializa, se trabaja como un puntero nulo. malloc anda pero realloc puede romper.
 } estructura_administrativa_de_marcos;
 
-//cosas a tener en cuenta: Deberia ser lo ultimo para terminar el TP aparte de mejorar la gestion de memoria en kernel y cpu, aunque en cpu y kernel no mucho ya que cpu tiene solo un proceso, no deberia usar mucha mem. 
+//cosas a tener en cuenta: 
 // y kernel solo debe borrar lo administrativo al final de un proceso ya que de la forma que esta el codigo nos evitamos tener multilpes estructuras administrativas clonadas solo controlar que no pase que las mismas se generen multiples veces
 
 // Hay que tener una estructura auxiliar, por proceso para gestionar el algoritmo de reemplazo y saber que marcos pertenecen a cada proceso HECHOOOO!!!
 // Tambien tiene que haber una especie de mapa con los marcos libres para manejarlos facilmente y no recorrer mucho buscandolos HECHOOOO!!!
+// Hay que armar el swap y ser capaces de escribir ahi con mmap HECHO!!!
 
-// Hay que armar el swap y ser capaces de escribir ahi con mmap
+// Y faltan los ALGOS DE remplazo + toda la liberacion de memoria TODO
 
-// Y faltan los ALGOS DE remplazo + toda la liberacion de memoria
 // Tenemos que ser capaces de volcar todo a swap en caso de que un proceso se suspenda y remover esas estructuras administrativas, 
 // luego cuando se pase a ready hay que recibir ninguna noti para cargar lo necesario a memoria
 
@@ -60,7 +63,7 @@ int main(){
 
 	tam_memoria = config_get_int_value(config, "TAM_MEMORIA");
 	tam_pagina = config_get_int_value(config, "TAM_PAGINA");
-	int cantidad_de_marcos_totales = tam_memoria / tam_pagina;
+	cantidad_de_marcos_totales = tam_memoria / tam_pagina;
 
 	estado_de_marcos = malloc(sizeof(uint32_t) * cantidad_de_marcos_totales); //1 es libre, 0 es ocupado. Esto no es de suma utilidad es solo mas conceptual al principio para tomar de aca el primer marco que encuentre libre para darselo a un proceso luego los reemplazos son locales y no vamos a tener un grado de multi que sea conflictivo con esto.
 	for(int i = 0; i < cantidad_de_marcos_totales; i++){
@@ -69,6 +72,8 @@ int main(){
 
 	path_swap = config_get_string_value(config, "PATH_SWAP");
 	marcos_por_proceso = config_get_int_value(config, "MARCOS_POR_PROCESO");
+
+	alg_reemplazo = config_get_string_value(config, "ALGORITMO_REEMPLAZO");
 
 	entradas_por_tabla = config_get_int_value(config, "ENTRADAS_POR_TABLA");
 	retardo_swap = config_get_int_value(config, "RETARDO_SWAP");
@@ -221,6 +226,9 @@ void crear_swap(int pid, int cantidad_de_marcos){
 	fclose(swap_file);
 }
 
+//Dezplazamiento es nro_pag * tam_pag
+
+
 void volcar_pagina_en_swap(uint32_t pid, uint32_t dezplazamiento, void* dato){
 	swap_struct* swap_map = (swap_struct *) dict_get(diccionario_swap, &pid);
 	memcpy(swap_map->swap_map + dezplazamiento, dato, tam_pagina);
@@ -235,7 +243,7 @@ void eliminar_swap(uint32_t pid, uint32_t memoria_total){
 	swap_struct* swap_map = (swap_struct *) dict_get(diccionario_swap, &pid);
 	munmap(swap_map->swap_map, memoria_total); 
 	
-	remove(swap_map->swap_map);
+	remove(swap_map->path_swap);
 	
 	free(swap_map->path_swap);
 	free(swap_map);
@@ -294,8 +302,12 @@ uint32_t crear_proceso(int tamanio_en_memoria, int pid){ // tengo que iniciar la
 	dict_add(diccionario_pid, &tabla_pags, &pid);
 
 	estructura_administrativa_de_marcos* admin = malloc(sizeof(estructura_administrativa_de_marcos));
+	admin->pagina_primer_nivel = tabla_pags;
 	admin->offset = 0;
 	admin->marcos_asignados = malloc(sizeof(int) * marcos_por_proceso);
+	for(int i = 0; i < marcos_por_proceso; i++){
+		admin->marcos_asignados[i] =-1;
+	}
 	dict_add(diccionario_marcos, &pid, admin);
 	
 	return tabla_pags;
@@ -383,7 +395,7 @@ int respuesta_a_pregunta_de_1er_acceso(int index_tabla, int entrada){
 
 
 
-int primer_marco_libre(estructura_administrativa_de_marcos est){
+int primer_marco_libre_del_proceso(estructura_administrativa_de_marcos* est){
 	int aux;
 	for(int i =0; i < marcos_por_proceso, i++){
 		if(est->marcos_asignados[est->offset] == -1){
@@ -397,22 +409,99 @@ int primer_marco_libre(estructura_administrativa_de_marcos est){
 	return -1 //esto ya que no hay marcos libres
 }
 
+int primer_marco_libre(){
+	for(int i=0; i< cantidad_de_marcos_totales, i++){
+		if(estado_de_marcos[i]==1)
+		return i; 
+	}
+	else{
+		//no deberia pasar nunca que se nos llene la memoria asi q ni lo miro
+	}
+}
+
+
 int respuesta_a_pregunta_de_2do_acceso(int index_tabla, int entrada, uint32_t pid){ // para esto me es mas util recibir el pid del proceso para poder agarrar sus estructuras administrativas
 	t_list* tabla_2do_nivel = list_get(lista_global_de_tablas_de_2do_nivel, index_tabla);
 	estructura_administrativa_de_marcos* estructura = (estructura_administrativa_de_marcos*)dict_get(diccionario_marcos, &pid);
-	
-	tabla_de_segundo_nivel* dato = list_get(tabla_2do_nivel, entrada); //cambiar el nombre de el struct
+	int marco_a_asignar;
+
+	tabla_de_segundo_nivel* dato = (tabla_de_segundo_nivel*) list_get(tabla_2do_nivel, entrada); //cambiar el nombre de el struct
 	if(dato->bit_presencia == 1){
 		return dato->marco;
-	}
+
+	}//setear correctamente los flags una vez hecha las cosas
 
 	else{
-		if((int marco = primer_marco_libre(estructura)) == -1){
-			//aca hay que implementar el reemplazo 
+		if((int marco = primer_marco_libre_del_proceso(estructura)) == -1){
+			marco_a_asignar = reemplazar_marco(estructura);//falta hacer lo que sigue
 		}
 		else{
-			//aca es que tenemos un marco libre
-		}
+			marco_a_asignar = primer_marco_libre();
+			estructura->marcos_asignados[marco] = marco_a_asignar;
+			estructura->offset = marco;
+			estructura->offset = estructura->offset % marcos_por_proceso;
+			estado_de_marcos[marco_a_asignar] = 0;
+			dato->marco = marco_a_asignar;
+		} //esto es si tiene marcos disponibles.
 	}
 	return dato->marco;
 }
+
+
+tabla_de_segundo_nivel* retornar_pagina(int indice_pagina, int marco_asignado){
+	t_list* tabla_1er_nivel = (t_list*)list_get(lista_global_de_tablas_de_1er_nivel, index_tabla);
+	int cantidad_pags - list_size(tabla_1er_nivel);
+	
+	for(int i = 0; i< cantidad_pags, i++){
+		
+		t_list* tabla_dos = (t_list*)list_get(tabla_1er_nivel, i);
+		int cant_entradas = list_size(tabla_dos);
+		
+		for(int c = 0; c< cant_entradas, c++){
+			tabla_de_segundo_nivel* dato = (tabla_de_segundo_nivel*) list_get(tabla_dos, c);
+			if(dato->marco == marco_asignado){
+				return dato;
+			}
+		}
+	}
+
+
+}
+
+int reemplazar_marco(estructura_administrativa_de_marcos* adm ){ //esto va a tener clock y clock-m
+
+	if(strcmp(alg_reemplazo, "CLOCK") == 0 ){
+		int iterador = 1;
+		
+		while(true){
+			
+			tabla_de_segundo_nivel* dato = retornar_pagina(adm->pagina_primer_nivel, adm->marcos_asignados[adm->offset]);
+					
+			if(dato->bit_de_uso ==0){
+				//reemplazar
+				return est->offset;
+			}
+			else{
+				dato->bit_de_uso = 0;
+			}
+
+		est->offset++;
+		est->offset = est->offset % marcos_por_proceso;
+		}
+	}
+	else{
+		//clock-m
+	}
+	
+}
+
+// a lo hora de cargar una pagina podemos cargarla de swap total esta todo en 0 leemos eso y fue asi nos facilitamos la vida
+
+// definir clock y clock m, por suerte como iteran sobre las estructuras administrativas es mas facil. podria poner el indice a la tabla nivel 1 en la estructura administrativa para facilitarnos la vida.
+
+
+//IMPLEMENTAR EL RETARDO BOLUDO ANTES DE LA RESPUESTA A CPU
+
+
+//ambos algos recorren las tablas de paginas buscando marcos libres solo que M le da prioridad a los que fueron modificados por sobre los que no para clock m hago un while que si es par busca 00 si es impar busca 01, seteando el flag de uso en 0.
+//hacer una funcion auxiliar que retorne la pagina que tiene un marco asignado.
